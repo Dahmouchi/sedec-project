a09153f347ddbd6abd603163c5c3f67f06e6b04a
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-10-29 15:19:00 +0100

    pushss

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,14 +3,14 @@
 
 import Layout from "@/components/layouts/Layout";
 import { getDictionary } from "./dictionaries";
-import Banner4 from "@/components/sections/Banner4";
-import Blog2 from "@/components/sections/Blog2";
+import Banner4 from "@/components/sections/Banner4"
+import Blog2 from "@/components/sections/Blog2"
 import Brand from "@/components/sections/Brand";
-import Newsletter from "@/components/sections/Newsletter";
-import Project4 from "@/components/sections/Project4";
+import Newsletter from "@/components/sections/Newsletter"
+import Project4 from "@/components/sections/Project4"
 import Roadmap from "@/components/sections/Roadmap";
-import Team from "@/components/sections/Team";
-import Video2 from "@/components/sections/Video2";
+import Team from "@/components/sections/Team"
+import Video2 from "@/components/sections/Video2"
 import Services from "@/components/sections/Services";
 import About from "@/components/sections/About";
 import Destination3 from "@/components/sections/Gallery";
@@ -24,20 +24,20 @@ export default async function Page({
   const dict = await getDictionary(lang); // en
   return (
     <>
-      <Layout headerStyle={7}>
-        <Banner4 />
-        <Video2 />
-        <About />
-        {/*<Brand />
+            <Layout headerStyle={7}>
+                <Banner4 />
+                <Video2 />
+                <About />
+                {/*<Brand />
                 <div className="section-pb-140"></div>*/}
-        <Project4 />
-        <Destination3 />
-        <Roadmap />
-        <Services />
-        {/*<Team />*/}
-        <Newsletter />
-        <Blog2 />
-      </Layout>
-    </>
+                <Project4 />
+                <Destination3 />
+                <Roadmap />
+                <Services />
+                 {/*<Team />*/}
+                <Newsletter />
+                <Blog2 />
+            </Layout>
+        </>
   );
 }
