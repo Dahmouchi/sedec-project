4a95b4b8d93ead9fb8e51269fb5505507848bdb2
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-11-13 14:57:56 +0100

    tep

--- src/components/sections/Test.tsx
+++ src/components/sections/Test.tsx
@@ -12,7 +12,7 @@ const Test = () => {
             <img
               src="/optimized/scondinave.webp"
               alt="Chalet Scandinavian"
-              className="w-full h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
+              className="w-[40vh] h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
             />
             <h4 className="font-serif font-semibold text-gray-500 hover:text-amber-800">
               Villas en Bois
@@ -26,7 +26,7 @@ const Test = () => {
             <img
               src="/optimized/charpente/charpente1.webp"
               alt="Chalet Toubkal"
-              className="w-full h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
+              className="w-[40vh] h-[27vh]  object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
             />
             <h4 className="font-serif font-semibold text-gray-500 hover:text-amber-800">
               Charpente
@@ -40,7 +40,7 @@ const Test = () => {
             <img
               src="/optimized/pergola4.webp"
               alt="Chalet M&M"
-              className="w-full h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
+              className="w-[40vh] h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
             />
             <h4 className="font-serif font-semibold text-gray-500 hover:text-amber-800">
               Pergola
@@ -49,12 +49,12 @@ const Test = () => {
         </li>
 
         {/* Chalet 4 */}
-        <li className="flex flex-col items-center text-center w-full lg:px-0 px-2">
+        <li className="flex flex-col items-center text-center w-full lg:px-0">
           <Link href="/batiments-modulaires" className="block">
             <img
               src="/optimized/merc/img176.webp"
               alt="Chalet Moroccan Touch"
-              className="w-full h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
+              className="w-[40vh] h-[27vh] object-cover rounded-lg mb-3 transition-transform duration-300 hover:scale-105"
             />
             <h4 className="font-serif font-semibold text-gray-500 hover:text-amber-800">
               Bâtiments Modulaires
